bbbb3e317d6a6a162782e1b901e5a55307501309
sazzadhossen360 <63567193+sazzadhossen360@users.noreply.github.com>
2020-04-16 13:15:43 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+(function ($){
+    "use strict";
+
+    jquery(document).ready(function($){
+
+        $(".video-play-btn").magnificPopUp();
+            type:'video',
+ 
+ 
+    });
+
+
+jquery(window).load(function($){
+
+});
+
+}(jquery));
